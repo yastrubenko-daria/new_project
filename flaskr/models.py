3662be89f6eb56d370a6480e5b611179635a4169
Yastrubenko Daria <yadakareeya@gmail.com>
2024-09-30 15:24:37 +0300

    added

---
+++ flaskr/models.py
@@ -0,0 +1,6 @@
+from django.db import  models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=200)
